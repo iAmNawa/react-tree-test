e25b71207a75d10d5649801f84abe07b2e4b38ef
Paul Borawski <paulborawski14@gmail.com>
2020-05-07 15:19:32 -0400

    expand and collapse working

--- src/components/TreeTest.js
+++ src/components/TreeTest.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import SortableTree from 'react-sortable-tree';
+import SortableTree, { toggleExpandedForAll } from "react-sortable-tree";
 import 'react-sortable-tree/style.css'; // This only needs to be imported once in your app
+import './TreeTest.css';
 
 export default class TreeTest extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      searchString: '',
       treeData: [
         { title: 'Paul', children: [{ title: 'Dog', children:[{title: 'Spot' },{title:'Daisy'}] }, {title: 'Cat'}] },
         { title: 'Jon', children: [{ title: 'Fish' }, { title: 'Snake' }] }
@@ -14,9 +16,36 @@ export default class TreeTest extends Component {
     };
   }
 
+  handleSearchOnChange = e => {
+    this.setState({
+      searchString: e.target.value
+    });
+  };
+
+  toggleNodeExpansion = expanded => {
+    this.setState(prevState => ({
+      treeData: toggleExpandedForAll({ treeData: prevState.treeData, expanded })
+    }));
+  };
+
   render() {
     return (
-      <div style={{ height: 400 }}>
+      <div style={{ height: '100vh' }}>
+        <button
+            name="expand"
+            onClick={this.toggleNodeExpansion.bind(this, true)}
+        >
+            Expand all
+        </button>
+        <button
+            name="collapse"
+            className="collapse"
+            onClick={this.toggleNodeExpansion.bind(this, false)}
+        >
+            Collapse all
+        </button>
+        <label>Search: </label>
+        <input onChange={this.handleSearchOnChange} />
         <SortableTree
           treeData={this.state.treeData}
           onChange={treeData => this.setState({ treeData })}
